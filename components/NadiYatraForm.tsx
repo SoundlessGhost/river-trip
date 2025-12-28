543f802051c55de00e35fe92b22e157eb74200df
Shahed Abid <shahedmk47@gmail.com>
2025-12-28 16:57:19 +0600

    --

--- components/NadiYatraForm.tsx
+++ components/NadiYatraForm.tsx
@@ -157,7 +157,7 @@ export default function NadiYatraForm() {
       childPrice = 800;
       infantPrice = 0;
     } else {
-      adultPrice = 10;
+      adultPrice = 1000;
       childPrice = 800;
       infantPrice = 0;
     }
